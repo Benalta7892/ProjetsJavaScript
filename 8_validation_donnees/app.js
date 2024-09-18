c4cdcdf03a3e8db405ea1c7d6adf3420faefd099
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-18 14:56:02 +0200

    Validation du mot de passe

--- 8_validation_donnees/app.js
+++ 8_validation_donnees/app.js
@@ -18,11 +18,11 @@ function showValidation({ index, validation }) {
   if (validation) {
     validationIcons[index].style.display = "inline";
     validationIcons[index].src = "ressources/check.svg";
-    validationTexts[index].style.display = "none";
+    if (validationTexts[index]) validationTexts[index].style.display = "none";
   } else {
     validationIcons[index].style.display = "inline";
     validationIcons[index].src = "ressources/error.svg";
-    validationTexts[index].style.display = "none";
+    if (validationTexts[index]) validationTexts[index].style.display = "none";
   }
 }
 
@@ -38,5 +38,53 @@ function mailValidation() {
     showValidation({ index: 1, validation: true });
   } else {
     showValidation({ index: 1, validation: false });
+  }
+}
+
+const pswInput = document.querySelector(".input-group:nth-child(3) input");
+
+pswInput.addEventListener("blur", passwordValidation);
+pswInput.addEventListener("input", passwordValidation);
+
+const passwordVerification = {
+  length: false,
+  symbol: false,
+  number: false,
+};
+
+const regexList = {
+  symbol: /[^a-zA-Z0-9\s]/,
+  number: /[0-9]/,
+};
+
+let passwordValue;
+
+function passwordValidation(e) {
+  passwordValue = pswInput.value;
+  let validationResult = 0;
+
+  for (const prop in passwordVerification) {
+    if (prop === "length") {
+      if (passwordValue.length < 6) {
+        passwordVerification.length = false;
+      } else {
+        passwordVerification.length = true;
+        validationResult++;
+      }
+      continue;
+    }
+
+    if (regexList[prop].test(passwordValue)) {
+      passwordVerification[prop] = true;
+      validationResult++;
+    } else {
+      passwordVerification[prop] = false;
+    }
+  }
+
+  if (validationResult !== 3) {
+    showValidation({ index: 2, validation: false });
+  } else {
+    showValidation({ index: 2, validation: true });
   }
 }
