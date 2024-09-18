2dc3a3e34df170c5c7b0fe495e07644cbf56d02b
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-18 10:14:58 +0200

    Gerer le decompte

--- 7_pomodoro/app.js
+++ 7_pomodoro/app.js
@@ -1,5 +1,5 @@
-let workTime = 1800;
-let restTime = 300;
+let workTime = 3;
+let restTime = 3;
 
 function formattedTime(time) {
   return `${Math.trunc(time / 60)}:${time % 60 < 10 ? `${time % 60}` : time % 60}`;
@@ -14,8 +14,19 @@ displayPause.textContent = formattedTime(restTime);
 const togglePlayBtn = document.querySelector(".toggle-btn");
 togglePlayBtn.addEventListener("click", togglePomodoro);
 
+let currentInterval = false;
+let timerID;
+
 function togglePomodoro() {
   hundlePlayPause();
+
+  if (currentInterval) return;
+  currentInterval = true;
+
+  workTime--;
+  displayWork.textContent = formattedTime(workTime);
+
+  timerID = setInterval(handleTicks, 1000);
 }
 
 let pause = true;
@@ -28,5 +39,26 @@ function hundlePlayPause() {
     pause = true;
     togglePlayBtn.firstElementChild.src = "ressources/play.svg";
     togglePlayBtn.setAttribute("data-toggle", "play");
+  }
+}
+
+const cycles = document.querySelector(".cycles");
+let cyclesNumber = 0;
+
+function handleTicks() {
+  if (!pause && workTime > 0) {
+    workTime--;
+    displayWork.textContent = formattedTime(workTime);
+  } else if (!pause && !workTime && restTime > 0) {
+    restTime--;
+    displayPause.textContent = formattedTime(restTime);
+  } else if (!pause && !workTime && !restTime) {
+    workTime = 3 - 1;
+    restTime = 3;
+    displayWork.textContent = formattedTime(workTime);
+    displayPause.textContent = formattedTime(restTime);
+
+    cyclesNumber++;
+    cycles.textContent = `Cycle(s) : ${cyclesNumber}`;
   }
 }
