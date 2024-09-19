fce8874b5ad653c8234c5aaf089d365ea5ce6f48
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-19 09:31:22 +0200

    Melange et selection des cartes

---
+++ 9_memory_card/app.js
@@ -0,0 +1,30 @@
+const cards = document.querySelectorAll(".card");
+
+function shuffleCards() {
+  cards.forEach((card) => {
+    const randomPos = Math.trunc(Math.random() * 12);
+    console.log(randomPos);
+    card.style.order = randomPos;
+  });
+}
+
+shuffleCards();
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
+
+let cardsPicked = [];
+function flipCard(e) {
+  saveCard(e.target.children[0], e.target.getAttribute("data-attr"));
+
+  if (cardsPicked.length === 2) result();
+}
+
+function saveCard(el, value) {
+  if (el === cardsPicked[0]?.el) return;
+
+  el.classList.add("active");
+  cardsPicked.push({ el, value });
+  console.log(cardsPicked);
+}
+
+function result() {}
