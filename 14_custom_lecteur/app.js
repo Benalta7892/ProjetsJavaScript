a6b1f1f785640f3ffcebce3b82ce4cadef5c50a7
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-22 12:19:28 +0200

    Gerer la range et le jeu de la video

---
+++ 14_custom_lecteur/app.js
@@ -0,0 +1,101 @@
+const video = document.querySelector(".video");
+const playToggler = document.querySelector(".play-toggler");
+const togglerImg = document.querySelector(".play-toggler img");
+
+video.addEventListener("click", togglePlay);
+playToggler.addEventListener("click", togglePlay);
+
+function togglePlay() {
+  if (video.paused) {
+    togglerImg.src = "ressources/pause.svg";
+    video.play();
+  } else {
+    togglerImg.src = "ressources/play.svg";
+    video.pause();
+  }
+}
+
+const timersDisplay = document.querySelectorAll(".time-display");
+
+video.addEventListener("loadeddata", fillDurationVariable); // Actualisation de la page de façon asynchrone
+window.addEventListener("load", fillDurationVariable); // Actualisation classique de la page
+
+let current;
+let totalDuration;
+
+function fillDurationVariable(e) {
+  if (Number.isNaN(video.duration)) return;
+  console.log(e.type);
+
+  current = video.currentTime;
+  totalDuration = video.duration;
+  console.log(totalDuration);
+
+  formatValue(current, timersDisplay[0]);
+  formatValue(totalDuration, timersDisplay[1]);
+
+  video.removeEventListener("loadeddata", fillDurationVariable);
+  window.removeEventListener("load", fillDurationVariable);
+}
+
+function formatValue(val, element) {
+  const currentMin = Math.trunc(val / 60);
+  let currentSec = Math.trunc(val % 60);
+
+  if (currentSec < 10) {
+    currentSec = `0${currentSec}`;
+  }
+
+  // if (currentMin < 10) {
+  //   currentMin = `0${currentMin}`;
+  // }
+
+  element.textContent = `${currentMin}:${currentSec}`;
+}
+
+const progress = document.querySelector(".progress");
+
+video.addEventListener("timeupdate", handleTimeUpdate);
+
+function handleTimeUpdate() {
+  current = video.currentTime;
+
+  formatValue(current, timersDisplay[0]);
+
+  // Pour avoir la position de la barre de progression
+  const progressPosition = current / totalDuration;
+  progress.style.transform = `scaleX(${progressPosition})`;
+
+  if (video.ended) {
+    togglerImg.src = "ressources/play.svg";
+  }
+}
+
+const muteBtn = document.querySelector(".mute-btn");
+const muteIcon = document.querySelector(".mute-btn img");
+
+muteBtn.addEventListener("click", handleMute);
+
+function handleMute() {
+  if (video.muted) {
+    video.muted = false;
+    muteIcon.src = "ressources/unmute.svg";
+  } else {
+    video.muted = true;
+    muteIcon.src = "ressources/mute.svg";
+  }
+}
+
+const volumeSlider = document.querySelector(".volume-slider");
+
+volumeSlider.addEventListener("change", handleVolumeModification);
+
+function handleVolumeModification() {
+  video.volume = volumeSlider.value / 100;
+
+  if (video.volume === 0) {
+    muteIcon.src = "ressources/mute.svg";
+  } else {
+    muteIcon.src = "ressources/unmute.svg";
+  }
+}
