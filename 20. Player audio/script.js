99b62082a915313d43d5198adb27cfcd6a0239a8
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-25 12:06:48 +0200

    Gerer la duree et le jeu de la musique

--- 20. Player audio/script.js
+++ 20. Player audio/script.js
@@ -4,3 +4,81 @@ const musicsData = [
   { title: "Aurora", artist: "SLUMB", id: 3 },
   { title: "Lost-Colours", artist: "Fakear", id: 4 },
 ];
+
+const musicPlayer = document.querySelector("audio");
+const musicTitle = document.querySelector(".music-title");
+const artistName = document.querySelector(".artist-name");
+const thumbnail = document.querySelector(".thumbnail");
+const indexTxt = document.querySelector(".current-index");
+
+let currentMusicIndex = 1;
+
+function populateUI({ title, artist }) {
+  musicTitle.textContent = title;
+  artistName.textContent = artist;
+  thumbnail.src = `./ressources//thumbs/${title}.png`;
+  musicPlayer.src = `./ressources/music/${title}.mp3`;
+  indexTxt.textContent = `${currentMusicIndex}/${musicsData.length}`;
+}
+
+populateUI(musicsData[currentMusicIndex - 1]);
+
+const playBtn = document.querySelector(".play-btn");
+playBtn.addEventListener("click", handlePlayPause);
+
+function handlePlayPause() {
+  if (musicPlayer.paused) {
+    play();
+  } else {
+    pause();
+  }
+}
+
+function play() {
+  playBtn.querySelector("img").src = "./ressources/icons/pause-icon.svg";
+  musicPlayer.play();
+}
+
+function pause() {
+  playBtn.querySelector("img").src = "./ressources/icons/play-icon.svg";
+  musicPlayer.pause();
+}
+
+const diplayCurrentTime = document.querySelector(".current-time");
+const durationTime = document.querySelector(".duration-time");
+const progressBar = document.querySelector(".progress-bar");
+
+musicPlayer.addEventListener("loadeddata", fillDurationVariables);
+
+let current;
+let totalDuration;
+
+function fillDurationVariables() {
+  current = musicPlayer.currentTime;
+  totalDuration = musicPlayer.duration;
+
+  formatValue(current, diplayCurrentTime);
+  formatValue(totalDuration, durationTime);
+}
+
+function formatValue(val, element) {
+  const currentMin = Math.trunc(val / 60);
+  let currentSec = Math.trunc(val % 60);
+
+  if (currentSec < 10) {
+    currentSec = `0${currentSec}`;
+  }
+
+  element.textContent = `${currentMin}:${currentSec}`;
+}
+
+musicPlayer.addEventListener("timeupdate", updateProgress);
+
+function updateProgress(e) {
+  current = e.srcElement.currentTime;
+  formatValue(current, diplayCurrentTime);
+
+  // 10 / 100 = 0.1
+  const progressValue = current / totalDuration;
+  progressBar.style.transform = `scaleX(${progressValue})`;
+}
